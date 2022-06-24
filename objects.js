80edd49d61517039acb780d42c8e0311ac237317
coach-stephanie <43859479+coach-stephanie@users.noreply.github.com>
2022-06-24 11:54:29 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -7,12 +7,11 @@
  *  the methods to change your object state. What object? Ask your coach.
  * 
  *  1. DEFINE: Make an object and set its properties, logging changes in between.
- *  2. MODIFY: Add 2 new properties and assing values. Change existing values,
- *     including adding or updating values inside arrays
- *  3. METHODS: Now its time to make some methods. Then can simply make a change
+ *  2. METHODS: Now its time to make some methods. Then can simply make a change
  *     to your values, like a boolean, or they can console log something about
  *     the object. Nothing fancy, unless you fancy it.
- *  4. LITERALLY: Use string literals to put it all together in one statement.
+ *  3. MODIFY: Change existing values, including adding or updating values inside arrays
+ *     using your methods.
  * 
  * *************************************************************************/
 
@@ -54,25 +53,21 @@ console.log(object)
 
 //************************************************************************* */
 
-
-
-console.log("------------------- CHALLENGE 2 : MODIFY   -------------------")
-
-//-->TODO: Log your object you created above
-
-
-//-->TODO: Update the object you just created  by adding new properties and values, including array elements, in this section.
-
-
-//-->TODO: Log your object again and observe changes
-
-
-console.log("------------------- CHALLENGE 3 : METHOD   -------------------")
+console.log("------------------- CHALLENGE 2 : METHODS   -------------------")
 
 //-->TODO: Add at least two methods (object functions) to your object defined in Challenge 1 and invoke them here.
 //          Make your methods update your variables, or add a random number to an array, etc.
 
 
-console.log("------------------- CHALLENGE 4 : LITERALLY   -------------------")
 
-//-->TODO: Put it all together using a string literal to tell the story of your object!
+
+
+console.log("------------------- CHALLENGE 3 : MODIFY   -------------------")
+
+//-->TODO: Update the object you just created by modifying values, including array elements, in this section.
+
+
+
+//-->TODO: Log your object again and observe changes
+
+
